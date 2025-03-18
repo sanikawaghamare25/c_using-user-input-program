19414909a6315175baa8e359be58502c683f7dcd
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:40:36 +0530

    Create surface Area Of Cube.c

---
+++ surface Area Of Cube.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int A,a;
+    
+    printf("Enter the a\n");
+    scanf("%d",&a);
+    printf("finding the A\n");
+    A=6*a*a;
+    printf("surface Area Of Cube is: %d",A);
+
+    return 0;
+}
