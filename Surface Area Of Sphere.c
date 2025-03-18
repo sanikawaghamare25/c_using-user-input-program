b3eceb2e197ded9d3f9d03d974f91369e43683c8
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:51:48 +0530

    Create Surface Area Of Sphere.c

---
+++ Surface Area Of Sphere.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float pi,r,A;
+    printf("Enter the pi\n");
+    scanf("%f",&pi);
+    printf("Enter the r\n");
+    scanf("%f",&r);
+    printf("finding the A\n\n");
+    A=4*3.14*r*r;
+    printf("Surface Area Of Sphere is:%f",A);
+
+    return 0;
+}
