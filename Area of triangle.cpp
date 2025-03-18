1cfb12091cffd38fe7273443c9b4d57f42a2f015
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:20:36 +0530

    Create Area of triangle.cpp

---
+++ Area of triangle.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Area of Triangle");
+    int b,h,Area;
+    printf("Enter the b\n");
+    scanf("%d",&b);
+    printf("Enter the h\n");
+    scanf("%d",&h);
+    printf("finding the Area \n");
+    Area=(b*h)/2;
+    printf("Area of Triangle is :%d",Area);
+
+    return 0;
+}
