081caa875e78e747c9fa3081d2f1604c568621a8
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:22:02 +0530

    Create Area of parallelogram.c

---
+++ Area of parallelogram.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Are of Parallelogram\n");
+    int base,height,Area;
+    printf("Enter the base :\n");
+    scanf("%d",&base);
+    printf("Enter the height :\n");
+    scanf("%d",&height);
+    
+    printf("finding the Area\n\n");
+    Area=base*height;
+    printf("Area of Parallelogram is:%d",Area);
+
+    return 0;
+}
