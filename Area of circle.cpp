d45703814fb991aa620b5cf3b4b000be7da5614c
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:19:32 +0530

    Create Area of circle.cpp

---
+++ Area of circle.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Area of ciercle\n");
+     float areaOfCircle,radius,pi=3.14;
+     
+     printf("Enter the pi\n");
+     scanf("%f",&pi);
+     printf("Enter the radius\n");
+     scanf("%f",&radius);
+     printf("finding the value: areaOfCircle\n");
+     areaOfCircle=3.14*radius*radius;
+     printf("Area of ciercle is:%f",areaOfCircle);
+
+    return 0;
+}
