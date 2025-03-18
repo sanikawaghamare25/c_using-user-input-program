5056cfef1e3010a3599f9ec8d8fba5c5466c52b4
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:28:17 +0530

    Create Area of square.c

---
+++ Area of square.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Area of square\n");
+    int side,Area;
+    printf("Enter the side\n");
+    scanf("%d",&side);
+    printf("finding the Area:\n\n");
+    Area=side*side;
+    printf("Area of square is : %d",Area);
+
+    return 0;
+}
