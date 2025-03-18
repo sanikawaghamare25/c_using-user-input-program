3d00bbf44e32120668eb94cfcb05c57fcd5bb8dd
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:35:41 +0530

    Create areaOfCircumferenceOfcircle.c

---
+++ areaOfCircumferenceOfcircle.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Area Of Circumference\n");
+    float pi,radius,areaOfCircumferenceOfCircle;
+    printf("Enter the pi\n");
+    scanf("%f",&pi);
+    printf("Ente the radius\n");
+    scanf("%f",&radius);
+    printf("finding the areaOfCircumferenceOfCircle \n");
+    areaOfCircumferenceOfCircle=2*3.14*radius;
+    printf("area Of Circumference Of Circle is: %f",areaOfCircumferenceOfCircle);
+    
+
+    return 0;
+}
