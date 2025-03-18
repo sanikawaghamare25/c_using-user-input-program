54946c3744acf91119972577d750dc881b347491
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:38:04 +0530

    Create volume of cube.c

---
+++ volume of cube.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int volume ,Area;
+    printf("Enter the Area\n");
+    scanf("%d",&Area);
+    printf("finding the volume\n");
+    volume=Area^3;
+    printf(" calculate Area of Cube is: %d",volume);
+
+    return 0;
+}
