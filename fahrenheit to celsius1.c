8edd0d2caf109d75d32922d074b5d24aa5cf953e
sanika waghamare <sanikawaghamare42@gmail.com>
2025-03-18 14:43:53 +0530

    Create fahrenheit to celsius1.c

---
+++ fahrenheit to celsius1.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int C,f;
+    printf("Enter the f\n");
+    scanf("%d",&f);
+    printf("finding the C\n");
+    C=(f-32)*5/9;
+    printf("fahrenheit to celsius is:%d",C);
+
+    return 0;
+}
